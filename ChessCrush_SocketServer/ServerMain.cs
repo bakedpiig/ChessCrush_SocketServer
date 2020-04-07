f68745a7c7cdc8eb49640e507dc753065840f73e
김태현 <allone1129@gmail.com>
2020-04-07 15:25:13 +0900

    Implement listen socket accept

--- ChessCrush_SocketServer/ServerMain.cs
+++ ChessCrush_SocketServer/ServerMain.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ChessCrush_SocketServer
 {
@@ -17,24 +19,58 @@ namespace ChessCrush_SocketServer
         {
             listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             listenSocket.Bind(new IPEndPoint(GetHostIP(), Port));
+            socketList.Add(listenSocket);
             listenSocket.Listen(ListenSocketBackLog);
-            Console.WriteLine($"Server IP: {GetHostIP().ToString()}, Port Number: {Port.ToString()}");
-            listenSocket.BeginAccept(ar => EndAccept(ar), null);
+            Console.WriteLine($"Server Opened! \nServer IP: {GetHostIP().ToString()}, Port Number: {Port.ToString()}");
 
             while (true) 
             {
-                Thread.Sleep(3000);
+                ArrayList listeningSocket = new ArrayList(socketList);
+
+                try
+                {
+                    Socket.Select(listeningSocket, null, null, 1000);
+                    foreach(var sock in listeningSocket)
+                    {
+                        if(sock == listenSocket)
+                        {
+                            var newSock = listenSocket.Accept();
+                            Console.WriteLine($"New Client Accepted! " +
+                             $"\nClient IP: {(newSock.RemoteEndPoint as IPEndPoint).Address}, Port: {(newSock.RemoteEndPoint as IPEndPoint).Port}");
+                            listenSocket.Listen(ListenSocketBackLog);
+                            socketList.Add(newSock);
+                        }
+                        else
+                        {
+                            var clientSocket = sock as Socket;
+                            byte[] buffer = new byte[256];
+                            clientSocket.Receive(buffer);
+                            InputMemoryStream inputMemoryStream = new InputMemoryStream(buffer);
+                            Task.Run(() =>
+                            {
+                                StreamRead(inputMemoryStream);
+                            });
+                        }
+                    }
+                    
+                    foreach(var sock in socketList)
+                    {
+                        if (!(sock == listenSocket || sock.Connected))
+                            socketList.Remove(sock);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    return;
+                }
             }
             listenSocket.Close();
         }
 
-        private static void EndAccept(IAsyncResult ar)
+        private static void StreamRead(InputMemoryStream stream)
         {
-            var newSock = listenSocket.EndAccept(ar);
-            listenSocket.BeginAccept(res => EndAccept(res), null);
-            Console.WriteLine($"New Client Accepted! " +
-                $"\nClient IP: {(newSock.RemoteEndPoint as IPEndPoint).Address}, Port: {(newSock.RemoteEndPoint as IPEndPoint).Port}");
-            socketList.Add(newSock);
+
         }
 
         private static IPAddress GetHostIP()
