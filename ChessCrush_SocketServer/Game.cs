b5b43d14741f489b0caf2df3e91ec523b2e067c9
김태현 <allone1129@gmail.com>
2020-04-07 20:56:41 +0900

    Define Game class

---
+++ ChessCrush_SocketServer/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessCrush_SocketServer
+{
+    public class Game
+    {
+        private List<string> waitingUserNameList;
+
+        public Game()
+        {
+            waitingUserNameList = new List<string>();
+        }
+
+        public void ParticipateGame(string userName)
+        {
+            waitingUserNameList.Add(userName);
+            
+            if(waitingUserNameList.Count>=2)
+            {
+                var newGameRoom = new GameRoom(waitingUserNameList[0], waitingUserNameList[1]);
+            }
+        }
+    }
+}
