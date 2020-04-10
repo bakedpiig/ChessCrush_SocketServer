19fe875a93e62473e9fa12d3f7e24b7593467d86
김태현 <allone1129@gmail.com>
2020-04-10 20:18:00 +0900

    Implement DBConnection

---
+++ ChessCrush_SocketServer/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ChessCrush_SocketServer
+{
+    public class DBConnection
+    {
+        private int dbPort;
+        private string uid;
+        private string password;
+        private string databaseName;
+        public MySqlConnection sqlConnection { get; private set; }
+
+        public DBConnection()
+        {
+            string conStr = $"server={"127.0.0.1"};port={dbPort.ToString()};uid={uid};pwd={password};database={databaseName};charset=utf8 ;";
+            sqlConnection = new MySqlConnection(conStr);
+        }
+    }
+}
