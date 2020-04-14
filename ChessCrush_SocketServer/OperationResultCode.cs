9a504c23ec2ba801789ee65f8b6afc932ef2314b
김태현 <allone1129@gmail.com>
2020-04-14 23:18:11 +0900

    Implement OperationResultCode

---
+++ ChessCrush_SocketServer/OperationResultCode.cs
@@ -0,0 +1,7 @@
+﻿namespace ChessCrush_SocketServer.OperationResultCode
+{
+    public enum SignUpCode { Success, UsingID }
+    public enum SignInCode { }
+    public enum ParticipateCode { }
+    public enum SendActionCode { }
+}
